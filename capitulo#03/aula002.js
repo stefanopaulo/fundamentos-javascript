2ed1bbe2422a62619636ebbb98dd6b1b97351965
Stefano Souza <stefano.desouza@vmetrix.com>
2021-11-02 16:05:59 -0300

    Boolean Types

---
+++ capitulo#03/aula002.js
@@ -0,0 +1,13 @@
+const canDrive = false
+
+if (canDrive) {
+    console.log('She can drive');
+}
+
+const accountBalance = 0
+
+if (!accountBalance) {
+    console.log(accountBalance);
+}
+
+console.log(!!accountBalance);
