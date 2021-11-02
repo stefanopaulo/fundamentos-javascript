033e148991df3724a621842f6ad89b195d061606
Stefano Souza <stefano.desouza@vmetrix.com>
2021-11-02 17:54:36 -0300

    Functions

---
+++ capitulo#04/aula004.js
@@ -0,0 +1,17 @@
+function nameOfFunction(paremeter1) {
+    // cod
+}
+
+function whatDayIsToday() {
+    const date = new Date()
+    console.log(`Today: ${date.getDate()}`);
+}
+
+whatDayIsToday()
+
+function sum(value1, value2) {
+    return value1 + value2
+}
+
+console.log(sum(1, 3))
+console.log(sum(10, 20))
