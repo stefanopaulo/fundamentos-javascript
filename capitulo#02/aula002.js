bc8f3a32c66eeb1a7a947ad0e8a8f4c37753651b
Stefano Souza <stefano.desouza@vmetrix.com>
2021-11-02 13:15:41 -0300

    Operators

---
+++ capitulo#02/aula002.js
@@ -0,0 +1,17 @@
+console.log("1 + 1 = ", 1+1);
+console.log("2 - 4 = ", 2-4);
+console.log("2 * 6 = ", 2*6);
+console.log("2 / 4 = ", 2/4);
+console.log("2 % 4 = ", 2%4);
+console.log("2 ** 4 = ", 2**4);
+
+console.log();
+console.log("--------------------------------");
+console.log();
+
+console.log("1 > 2 = ", 1 > 2);
+console.log("2 < 4 = ", 2 < 4);
+console.log("1 === 2 = ", 1 === 2);
+console.log("1 !== 2 = ", 1 !== 2);
+console.log("1 >= 1 = ", 1 >= 1);
+console.log("1 <= 1 = ", 1 <= 1);
