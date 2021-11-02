82fdb1d16eaebdbfe1e5b8d8d0fd0f25db89f809
Stefano Souza <stefano.desouza@vmetrix.com>
2021-11-02 17:34:15 -0300

    Predefined global objects

---
+++ capitulo#04/aula002.js
@@ -0,0 +1,4 @@
+console.log('Keys: ' + Object.keys({ name: 'Stefano' }));
+console.log('Values: ' + Object.values({ name: 'Stefano' }));
+console.log('Now: ' + Date.now());
+console.log('Random: ' + Math.random());
